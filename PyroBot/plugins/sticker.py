55a4f5caf451e190fe35369f049c8cac74b06714
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-06-22 23:29:18 +0530

    Delete PyroBot directory

--- PyroBot/plugins/sticker.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.command(["start"]))
-async def start(bot, update):
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text="Hello there!",
-        reply_to_message_id=update.message_id
-    )
