376f7725671c47191bfa430fc2b901d9f0f10987
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-06-25 14:15:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,13 +51,14 @@ async def stickerid(bot, message):
     if message.sticker:
        await message.reply(f"**Sticker ID is**  \n `{message.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{message.sticker.file_unique_id}`", quote=True)
 
-@bughunter0.on_message(filters.command(["test"]))
-async def getsticker(bot, message):
+@bughunter0.on_message(filters.command(["getsticker"]) & filters.reply)
+async def getsticker(bot, update):
      message.reply("Testing functions")
      if message.sticker:
         fileid = message.sticker.file_id
         await bot.download_media(message=update,file_name="sticker.png",file_id=fileid)
         response = upload_file("sticker.png")
+        message.reply("This works fine")
         try:
              os.remove(sticker.png)
         except:
