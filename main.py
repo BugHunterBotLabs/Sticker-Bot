2c460a1303b844ee81c573e1e2d7d10635b0ffa0
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-06-26 23:09:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,8 +49,8 @@ async def ping(bot, message):
 
 @bughunter0.on_message(filters.command(["download"]))
 async def getsticker(bot, message):  
-    random_id = random.randint(100,1000)
-    message = message.message_id
+    random_id = random.randint(100,1000)  
+    message=update 
     tx = await message.reply_text("Checking Sticker")
     if message.sticker:
        await tx.edit("Downloading")
