7b063e42c8f6f4eb282ca141cd0091db2f905337
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-06-28 11:16:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ async def clearcache(bot, message):
 @bughunter0.on_message(filters.command(["stickerid"]))
 async def stickerid(bot, message):   
     if message.reply_to_message.sticker:
-       await message.reply(f"**Sticker ID is**  \n `{message.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{message.sticker.file_unique_id}`", quote=True)
+       await message.reply(f"**Sticker ID is**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
     else: 
        await message.reply("Oops !! Not a sticker file")
  
