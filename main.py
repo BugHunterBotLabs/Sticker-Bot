4837c206a83991d7940dd6e40bbae44417236a1c
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-06-29 22:17:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -108,8 +108,7 @@ async def clearcache(bot, message):
     await txt.edit("Cleared "+ str(i) + "File") 
     await txt.delete()
     
-# @bughunter0.on_message(filters.command(["stickerid"]))
-@bughunter0.on_message(filters.sticker)
+@bughunter0.on_message(filters.command(["stickerid"]))
 async def stickerid(bot, message):   
     if message.reply_to_message.sticker:
        await message.reply(f"**Sticker ID is**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
