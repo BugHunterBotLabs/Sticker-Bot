d57b67f7fee9e584803c208a001581ed18f03497
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-07-07 14:30:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,40 +59,41 @@ async def ping(bot, message):
 async def getsticker(bot, message):  
     tx = await message.reply_text("Checking Sticker")
     await tx.edit("Validating sticker..")
-    await tx.delete()
-    if message.reply_to_message is None: 
-            tx =  await tx.edit("Reply to a Sticker File!")       
+    if message.reply_to_message.sticker is False:
+        await tx.edit("Not a Sticker File!!")
     else :
-          if message.reply_to_message.sticker.is_animated:
-             try :
-                   
-                   tx = await message.reply_text("Downloading...")
-                   file_path = DOWNLOAD_LOCATION + f"{message.chat.id}.tgs"
-                   await message.reply_to_message.download(file_path)  
-                   await tx.edit("Downloaded") 
-                #   zip_path= ZipFile.write("")
-                   await tx.edit("Uploading...")
-                   start = time.time()
-                   await message.reply_document(file_path,caption="©@BugHunterBots")
-                   await tx.delete()   
-                   os.remove(file_path)
-                #   os.remove(zip_path)
-             except Exception as error:
-                   print(error)
+          if message.reply_to_message is None: 
+               tx =  await tx.edit("Reply to a Sticker File!")       
+          else :
+               if message.reply_to_message.sticker.is_animated:
+                   try :     
+                        tx = await message.reply_text("Downloading...")
+                        file_path = DOWNLOAD_LOCATION + f"{message.chat.id}.tgs"
+                        await message.reply_to_message.download(file_path)  
+                        await tx.edit("Downloaded") 
+                    #   zip_path= ZipFile.write("")
+                        await tx.edit("Uploading...")
+                        start = time.time()
+                        await message.reply_document(file_path,caption="©@BugHunterBots")
+                        await tx.delete()   
+                        os.remove(file_path)
+                    #   os.remove(zip_path)
+                   except Exception as error:
+                        print(error)
  
-          elif message.reply_to_message.sticker.is_animated is False:        
-             try : 
-                   tx = await message.reply_text("Downloading...")
-                   file_path = DOWNLOAD_LOCATION + f"{message.chat.id}.png"
-                   await message.reply_to_message.download(file_path)   
-                   await tx.edit("Downloaded")
-                   await tx.edit("Uploading...")
-                   start = time.time()
-                   await message.reply_document(file_path,caption="©@BugHunterBots")
-                   await tx.delete()   
-                   os.remove(file_path)
-             except Exception as error:
-                   print(error)
+              elif message.reply_to_message.sticker.is_animated is False:        
+                   try : 
+                       tx = await message.reply_text("Downloading...")
+                       file_path = DOWNLOAD_LOCATION + f"{message.chat.id}.png"
+                       await message.reply_to_message.download(file_path)   
+                       await tx.edit("Downloaded")
+                       await tx.edit("Uploading...")
+                       start = time.time()
+                       await message.reply_document(file_path,caption="©@BugHunterBots")
+                       await tx.delete()   
+                       os.remove(file_path)
+                  except Exception as error:
+                       print(error)
 
 @bughunter0.on_message(filters.private & filters.command(["clearcache"]))
 async def clearcache(bot, message):   
